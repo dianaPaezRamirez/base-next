1fb5ed0155b10f529d52f76d42486a3c1371e9d4
Dianapaez <dpaez@serempre.com>
2025-03-14 10:37:26 -0500

    add fist files

---
+++ src/app/provider.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+//import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import { useState } from "react";
+
+// TODO: Create a custom provider for every provider
+export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    //<SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          {children}
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ReactQueryStreamedHydration>
+    </QueryClientProvider>
+    // </SessionProvider>
+  );
+};
